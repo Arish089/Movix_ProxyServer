46e428cef790c3d897e3cee270d6aeee5a01e8b0
Arish089 <152006719+Arish089@users.noreply.github.com>
2024-06-29 13:49:12 +0530

    done

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/api/data',async (req,res)=>{
       }
 )
 
-app.get('/api/:id',async (req,res)=>{
+app.get('/api/movie/:id',async (req,res)=>{
   const {id} = req.params
   console.log(id);
         try {
@@ -58,6 +58,44 @@ app.get('/api/:id',async (req,res)=>{
         console.log(resp1,resp1.data);
           res.status(200).send({
             movie:resp1.data,
+            credits:resp2.data})
+        } catch (error) {
+          console.error('error fetching movie data',error.message)
+          res.status(error.response ? error.response.status: 500).send({
+            message: error.message,
+      status: error.response ? error.response.status : 500
+          })
+        }
+      }
+)
+
+app.get('/api/tv/:id',async (req,res)=>{
+  const {id} = req.params
+  console.log(id);
+        try {
+          const [resp1,resp2] = await Promise.all([
+             axios({
+            method:'get',
+            baseURL:`https://api.themoviedb.org/3`,
+            url:`/movie/${id}?language=en-US`,
+            headers:{
+              'accept': 'application/json',
+              'Authorization':`Bearer ${process.env.API_KEY}` 
+            }
+          }),
+          axios({
+            method:'get',
+            baseURL:`https://api.themoviedb.org/3`,
+            url:`/movie/${id}/credits?language=en-US&append_to_response=videos`,
+            headers:{
+              'accept': 'application/json',
+              'Authorization':`Bearer ${process.env.API_KEY}` 
+            } 
+          })
+        ])
+        console.log(resp1,resp1.data);
+          res.status(200).send({
+            tv:resp1.data,
             credits:resp2.data})
         } catch (error) {
           console.error('error fetching movie data',error.message)
