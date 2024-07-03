643a8a46933e12f08ab1d6f8abff4f5f510c7ca1
Arish089 <152006719+Arish089@users.noreply.github.com>
2024-07-03 10:58:25 +0530

    solved a bug

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,27 +6,32 @@ const UserRouter = Router();
 
 UserRouter.post('/signup',async (req,res)=>{
     const {email,name} = req.body
-    const newUser = new UserModel({email,name})
-    await UserModel.create(newUser)
-    res.status(200).json({message:"User registered successfully"})
+    try {
+        const newUser = new UserModel({ email, name });
+        await UserModel.create(newUser);
+        res.status(200).json({ message: "User registered successfully" });
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
 })
 
 UserRouter.post('/login',async (req,res)=>{
     const {email,name} = req.body
+    console.log(email,name);
     try {
         const user = await UserModel.findOne({email})
+        console.log(user);
         if(!user){
             const newUser = new UserModel({email,name})
             await UserModel.create(newUser)
             res.status(200).json({message:"User registered successfully"})
+        }else{
+            res.status(201).send('User already exists')
         }
 
     } catch (error) {
         res.status(500).send({message: error})
     }
-    const newUser = new UserModel({email,name})
-    await UserModel.create(newUser)
-    res.status(200).json({message:"User registered successfully"})
 })
 
 module.exports = UserRouter
